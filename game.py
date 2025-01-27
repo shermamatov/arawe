647c5172d5dcf49bb6360678a418dcb96ad50cf8
d4yr41n <d4yr41n@gmail.com>
2025-01-27 07:40:25 +0000

    update

--- game.py
+++ game.py
@@ -1,27 +1,26 @@
-class Player:
-    speed = 2
+class Object:
+    x: float
+    y: float
+
+
+class Shell(Object):
+    dx = 0
+    dy = 0
+
+
+class Player(Shell):
+    speed = 1
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def move_forward(self):
-        self.y -= self.speed
+    def move(self, vector_x, vecotr_y):
+        self.x += vector_x * self.speed
+        self.y += vector_y * self.speed
 
-    def move_left(self):
-        self.x -= self.speed
-
-    def move_right(self):
-        self.x += self.speed
-
-    def move_backward(self):
-        self.y += self.speed
 
 class Game:
     width = 3200
     height = 3200
-    player = Player(128, 128)
-
-    def get_state(self):
-        return f"player: {self.player.x} {self.player.y}"
-        
+    registry: list[Player] = []
