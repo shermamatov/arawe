1da6a587a09bd0ce337241572352d541959506a1
d4yr41n <d4yr41n@gmail.com>
2025-01-27 17:16:01 +0000

    update

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+from random import randint 
 
 from starlette.websockets import WebSocket
 
@@ -15,15 +16,29 @@ class Shell(Object):
 
 class Player(Shell):
     speed = 1
+    id = 0
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    @classmethod
+    def get_id(cls):
+        cls.id += 1
+        return cls.id
+
+    def __str__(self):
+        return 'p' + str(self.id)
+
+    def __init__(self):
+        self.x = randint(64, Game.width - 64)
+        self.y = randint(64, Game.height - 64)
+        self.id = self.get_id()
 
     def move(self):
-        self.x += self.dx * self.speed
-        self.y += self.dy * self.speed
-        return self.x, self.y
+        if self.dx or self.dy:
+            self.x += self.dx * self.speed
+            self.y += self.dy * self.speed
+            return self.x, self.y
+
+    def get_pos(self):
+        return f"{self}pos:{self.x},{self.y}"
 
     def set_vector(self, dx, dy):
         self.dx = dx
@@ -31,17 +46,37 @@ class Player(Shell):
 
 
 class Game:
-    width = 3200
-    height = 3200
-    registry: list[Player] = []
+    width = 640
+    height = 640
+    players: dict[WebSocket, Player] = {}
 
-    def loop(self):
+    async def add_player(self, ws: WebSocket):
+        self.players[ws] = player = Player()
+        await ws.send_text(player.get_pos())
 
-        for p in self.registry:
-            p.move()
-        # send to clients
-        await sleep(33)
+    async def del_player(self, ws: WebSocket):
+        player = self.players[ws].pop(ws)
+        self.send_all(f"{player}hp:0")
 
-    async def send_clients(self, msg):
-        WebSocket
-        
+    async def send_all(self, msg: str):
+        for ws in self.players:
+            await ws.send_text(msg)
+
+    async def loop(self):
+        while True:
+            for player in self.players.values():
+                pos = player.move()
+                if pos:
+                    x, y = pos
+                    await self.send_all(f"{player}pos:{x},{y}")
+            await sleep(33)
+
+    async def set_vector(self, ws, dx, dy):
+        player = self.players[ws]
+        player.set_vector(dx, dy)        
+        self.send_all(f"{player}vec:{dx},{dy}")
+
+    async def use(self, ws, dx=None, dy=None):
+        player = self.players[ws]
+        self.send_all(f"{player}use")
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from starlette.applications import Starlette
 from starlette.responses import FileResponse
 from starlette.routing import Route, Mount, WebSocketRoute
@@ -16,15 +18,39 @@ async def index(request):
 
 class Server(WebSocketEndpoint):
     encoding = "text"
-    websockets = []
 
-    async def on_connect(self, websocket):
+    async def on_connect(self, ws):
         await websocket.accept()
-        await websocket.send_text(game.get_state())
+        await game.add_player(ws)
 
-    async def on_receive(self, websocket, data):
-        print()
-        pass
+    async def on_receive(self, ws, data):
+        if data.startswith("vec"):
+            msg = data.split(':')
+            if len(msg) != 2:
+                return
+            cmd, args = msg[0], msg[1].split(',')
+            if len(args) != 2:
+                return
+            dx, dy = args
+            if not dx.isdigit() or not dy.isdigit():
+                return
+            dx = int(dx)
+            dy = int(dy)
+            for i in dx, dy:
+                if i < -1 or i > 1:
+                    return
+            await game.set_vector(ws, dx, dy)
+        elif data == "use":
+            await game.use(ws)
+    
+    async def on_disconnect(self, ws, close_code):
+        await game.del_player(ws)
+
+
+@asynccontextmanager
+async def lifespan(app):
+    await game.loop()
+    yield
 
 
 app = Starlette(
