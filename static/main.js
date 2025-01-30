4944f6f847fdd1a24a4e9b4a5b909aa7d6b66d7b
unknown <timurshermamatov1@gmail.com>
2025-01-30 22:03:05 +0600

    fix bugs

--- static/main.js
+++ static/main.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
 ctx.scale(4, 4);
 
-const socket = new WebSocket("wss://arawe-production.up.railway.app/ws");
+const socket = new WebSocket("ws://" + document.location.host);
 let frame = 0;
 //!  constants end
 
